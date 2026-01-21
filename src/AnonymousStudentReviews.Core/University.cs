439fd85124bd7187afa3078fc139b0cc1b9f8ddc
pichus <dimatyzuk@gmail.com>
2026-01-21 16:18:45 +0200

    make city and website columns of universities table nullable

--- src/AnonymousStudentReviews.Core/University.cs
+++ src/AnonymousStudentReviews.Core/University.cs
@@ -4,6 +4,8 @@ public class University
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
-    public string City { get; set; }
-    public string Website { get; set; }
+    public string? City { get; set; }
+    public string? Website { get; set; }
+
+    // public ICollection<User> Users { get; set; }
 }
