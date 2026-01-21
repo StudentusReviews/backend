3f238f031d872076ff566cb8e5e50c9086b62cff
pichus <dimatyzuk@gmail.com>
2026-01-21 20:11:36 +0200

    add AddAllowedEmailDomainEntity migration

--- src/AnonymousStudentReviews.Infrastructure/Data/Migrations/20260121181004_AddAllowedEmailDomainEntity.Designer.cs
+++ src/AnonymousStudentReviews.Infrastructure/Data/Migrations/20260121181004_AddAllowedEmailDomainEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AnonymousStudentReviews.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AnonymousStudentReviews.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260121181004_AddAllowedEmailDomainEntity")]
+    partial class AddAllowedEmailDomainEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -41,24 +44,6 @@ namespace AnonymousStudentReviews.Infrastructure.Data.Migrations
                     b.HasIndex("UniversityId");
 
                     b.ToTable("allowed_email_domains", (string)null);
-                });
-
-            modelBuilder.Entity("AnonymousStudentReviews.Core.DummyAggregate.Dummy", b =>
-                {
-                    b.Property<long>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("bigint");
-
-                    NpgsqlPropertyBuilderExtensions.UseIdentityByDefaultColumn(b.Property<long>("Id"));
-
-                    b.Property<string>("Name")
-                        .IsRequired()
-                        .HasMaxLength(20)
-                        .HasColumnType("character varying(20)");
-
-                    b.HasKey("Id");
-
-                    b.ToTable("dummies", (string)null);
                 });
 
             modelBuilder.Entity("AnonymousStudentReviews.Core.Role", b =>
---
+++ src/AnonymousStudentReviews.Infrastructure/Data/Migrations/20260121181004_AddAllowedEmailDomainEntity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace AnonymousStudentReviews.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAllowedEmailDomainEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "allowed_email_domains",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UniversityId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Domain = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_allowed_email_domains", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_allowed_email_domains_universities_UniversityId",
+                        column: x => x.UniversityId,
+                        principalTable: "universities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_allowed_email_domains_UniversityId",
+                table: "allowed_email_domains",
+                column: "UniversityId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "allowed_email_domains");
+        }
+    }
+}
