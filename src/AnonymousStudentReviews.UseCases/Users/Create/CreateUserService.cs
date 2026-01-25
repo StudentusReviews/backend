1890d567eee5734289645de945adc0d15bf34f50
pichus <dimatyzuk@gmail.com>
2026-01-25 18:54:57 +0200

    auto assign student role to user during registration draft

--- src/AnonymousStudentReviews.UseCases/Users/Create/CreateUserService.cs
+++ src/AnonymousStudentReviews.UseCases/Users/Create/CreateUserService.cs
@@ -2,6 +2,7 @@ using System.Net.Mail;
 
 using AnonymousStudentReviews.Core.Abstractions;
 using AnonymousStudentReviews.Core.Aggregates.AllowedEmailDomain;
+using AnonymousStudentReviews.Core.Aggregates.Role;
 using AnonymousStudentReviews.Core.Aggregates.User;
 
 using Microsoft.Extensions.Logging;
@@ -14,12 +15,14 @@ public class CreateUserService : ICreateUserService
     private readonly IEmailHasher _emailHasher;
     private readonly ILogger<CreateUserService> _logger;
     private readonly IPasswordHasher _passwordHasher;
+    private readonly IRoleRepository _roleRepository;
     private readonly IUnitOfWork _unitOfWork;
     private readonly IUserRepository _userRepository;
 
     public CreateUserService(IAllowedEmailDomainRepository allowedEmailDomainRepository, IEmailHasher emailHasher,
         IPasswordHasher passwordHasher,
-        IUserRepository userRepository, IUnitOfWork unitOfWork, ILogger<CreateUserService> logger)
+        IUserRepository userRepository, IUnitOfWork unitOfWork, ILogger<CreateUserService> logger,
+        IRoleRepository roleRepository)
     {
         _allowedEmailDomainRepository = allowedEmailDomainRepository;
         _emailHasher = emailHasher;
@@ -27,12 +30,13 @@ public class CreateUserService : ICreateUserService
         _userRepository = userRepository;
         _unitOfWork = unitOfWork;
         _logger = logger;
+        _roleRepository = roleRepository;
     }
 
     public async Task<Result<User>> HandleAsync(CreateUserDto dto)
     {
         _logger.LogInformation("Create user service started");
-        
+
         var emailHash = _emailHasher.Hash(dto.Email);
 
         if (await _userRepository.UserWithEmailHashExistsAsync(emailHash))
@@ -58,7 +62,17 @@ public class CreateUserService : ICreateUserService
             var universityId =
                 (await _allowedEmailDomainRepository.FindByDomainAsync(emailDomain)).Value.UniversityId;
             createdUser.UniversityId = universityId;
-            // createdUser.Roles.Add();
+
+            var getRoleResult = await _roleRepository.GetRoleByNameAsync(RoleNameConstants.Student);
+
+            if (getRoleResult.IsFailure)
+            {
+                throw new InvalidOperationException("No role named \"Student\" found in the db");
+            }
+
+            var role = getRoleResult.Value;
+            
+            createdUser.Roles.Add(role);
         }
 
         _userRepository.CreateUser(createdUser);
