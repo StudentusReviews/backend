320748c0332105232f60d26462214ab3c3523778
pichus <dimatyzuk@gmail.com>
2026-01-26 18:11:31 +0200

    implement email verification token generator

---
+++ src/AnonymousStudentReviews.Infrastructure/EmailVerificationToken/EmailVerificationTokenGenerator.cs
@@ -0,0 +1,19 @@
+using System.Security.Cryptography;
+
+using AnonymousStudentReviews.UseCases.Users.Create;
+
+namespace AnonymousStudentReviews.Infrastructure.EmailVerificationToken;
+
+public class EmailVerificationTokenGenerator : IEmailVerificationTokenGenerator
+{
+    public string Generate()
+    {
+        var bytes = RandomNumberGenerator.GetBytes(64);
+        var token = Convert.ToBase64String(bytes)
+            .Replace("+", "-")
+            .Replace("/", "_")
+            .TrimEnd('=');
+
+        return token;
+    }
+}
