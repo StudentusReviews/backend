f1a2902ed0adf58c85f026bf5581c8489ff759eb
Zlata Dovbyk <dovbykzlata@gmail.com>
2026-02-10 21:45:18 +0200

    add review response model

---
+++ src/AnonymousStudentReviews.Api/Features/Reviews/Common/ReviewResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace AnonymousStudentReviews.Api.Features.Reviews.Common;
+
+public class ReviewResponse
+{
+    public string Id { get; set; } = string.Empty;
+    public string UniversityId { get; set; } = string.Empty;
+    public string UserId { get; set; } = string.Empty;
+    public int Score { get; set; }
+    public string Body { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
