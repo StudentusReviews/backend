a4204ee4ca0d126c4fc1111b80be210668bd64b0
Zlata Dovbyk <dovbykzlata@gmail.com>
2026-02-11 11:25:13 +0200

    add create/edit/delete endpoints contracts and validation

---
+++ src/AnonymousStudentReviews.Api/Features/Reviews/Edit/EditReviewController.cs
@@ -0,0 +1,48 @@
+﻿using AnonymousStudentReviews.Api.Extensions;
+using AnonymousStudentReviews.Api.Features.Reviews.Common;
+
+using FluentValidation;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using OpenIddict.Validation.AspNetCore;
+
+namespace AnonymousStudentReviews.Api.Features.Reviews.Edit;
+
+[Authorize(
+    AuthenticationSchemes = OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme,
+    Roles = "Student")]
+[ApiController]
+[Route("api/reviews")]
+public class EditReviewController : ControllerBase
+{
+    private readonly IValidator<EditReviewRequest> _validator;
+    
+    public EditReviewController(IValidator<EditReviewRequest> validator)
+    {
+        _validator = validator;
+        
+    }
+
+    [HttpPut("{reviewId:guid}")]
+    [ProducesResponseType(typeof(ReviewResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status501NotImplemented)]
+    public async Task<ActionResult<ReviewResponse>> Edit([FromRoute] Guid reviewId, [FromBody] EditReviewRequest request)
+    {
+        var validationResult = await _validator.ValidateAsync(request);
+
+        if (!validationResult.IsValid)
+        {
+            return validationResult.ToProblemDetails(Request.Path);
+        }
+
+        return StatusCode(StatusCodes.Status501NotImplemented);
+        
+    }
+    
+}
---
+++ src/AnonymousStudentReviews.Api/Features/Reviews/Edit/EditReviewRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace AnonymousStudentReviews.Api.Features.Reviews.Edit;
+
+public class EditReviewRequestValidator : AbstractValidator<EditReviewRequest>
+{
+    public EditReviewRequestValidator()
+    {
+        RuleFor(x => x.Score).InclusiveBetween(1, 10);
+        RuleFor(x => x.Body).NotEmpty().MinimumLength(20).MaximumLength(2000);
+    }
+}
