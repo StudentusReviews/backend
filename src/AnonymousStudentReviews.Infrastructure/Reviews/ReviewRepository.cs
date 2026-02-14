92077cf7e4ce152fa3cf85e00e4213c0ad68c783
Zlata Dovbyk <dovbykzlata@gmail.com>
2026-02-14 18:43:08 +0200

    add review repository

---
+++ src/AnonymousStudentReviews.Infrastructure/Reviews/ReviewRepository.cs
@@ -0,0 +1,32 @@
+﻿using AnonymousStudentReviews.Core.Aggregates.Review;
+using AnonymousStudentReviews.Infrastructure.Data;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace AnonymousStudentReviews.Infrastructure.Reviews;
+
+public class ReviewRepository : IReviewRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public ReviewRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public void Create(Review review)
+    {
+        _dbContext.Set<Review>().Add(review);
+    }
+
+    public async Task<Review?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        return await _dbContext.Set<Review>()
+            .FirstOrDefaultAsync(r => r.Id == id, cancellationToken);
+    }
+
+    public void Delete(Review review)
+    {
+        _dbContext.Set<Review>().Remove(review);
+    }
+}
