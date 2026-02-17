6614d6e983415f1d84a0ccadf65283b464053fee
pichus <dimatyzuk@gmail.com>
2026-02-18 00:58:58 +0200

    migrate

---
+++ src/AnonymousStudentReviews.Infrastructure/Data/Migrations/ApplicationDatabase/20260217225613_AddRelationShipBetweenUniversitiesAndReviews.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnonymousStudentReviews.Infrastructure.Data.Migrations.ApplicationDatabase
+{
+    /// <inheritdoc />
+    public partial class AddRelationShipBetweenUniversitiesAndReviews : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddForeignKey(
+                name: "FK_reviews_universities_UniversityId",
+                table: "reviews",
+                column: "UniversityId",
+                principalTable: "universities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_reviews_universities_UniversityId",
+                table: "reviews");
+        }
+    }
+}
