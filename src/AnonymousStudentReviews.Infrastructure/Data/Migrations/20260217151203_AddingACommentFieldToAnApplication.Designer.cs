33ecba5b8826217edc96fba6fc90055d123f0ce9
Yaroslav <kucheryaroslav2@gmail.com>
2026-02-18 18:06:42 +0200

    .

--- src/AnonymousStudentReviews.Infrastructure/Data/Migrations/20260217151203_AddingACommentFieldToAnApplication.Designer.cs
+++ src/AnonymousStudentReviews.Infrastructure/Data/Migrations/20260217151203_AddingACommentFieldToAnApplication.Designer.cs
@@ -11,7 +11,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace AnonymousStudentReviews.Infrastructure.Data.Migrations
 {
-    [DbContext(typeof(ApplicationDbContext))]
+    [DbContext(typeof(ApplicationDatabaseContext))]
     [Migration("20260217151203_AddingACommentFieldToAnApplication")]
     partial class AddingACommentFieldToAnApplication
     {
