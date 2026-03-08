8273d42149c57d1a987ea13c705a20f604c348b4
pichus <dimatyzuk@gmail.com>
2026-03-08 19:00:45 +0200

    add proper validation and error messages to cors options

--- src/AnonymousStudentReviews.Api/Options/CorsOptions.cs
+++ src/AnonymousStudentReviews.Api/Options/CorsOptions.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AnonymousStudentReviews.Api.Options;
 
 public class CorsOptions
 {
     public const string SectionName = "Cors";
 
-    public List<string> AllowedOrigins { get; set; } = new();
+    [Required(ErrorMessage = "Missing configuration value for 'Cors:AllowedOrigins'.")]
+    [MinLength(1, ErrorMessage = "At least one allowed origin must be configured in 'Cors:AllowedOrigins'.")]
+    public List<string> AllowedOrigins { get; init; } = new();
 }
