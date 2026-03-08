23e791e4fcf8979a9f38a4fe230e6febb4836e58
pichus <dimatyzuk@gmail.com>
2026-03-08 18:38:08 +0200

    add account verification options validation + meaningful error messages + substitute EmailVerificationTokenExpirationHours with EmailVerificationTokenExpirationMinutes

--- src/AnonymousStudentReviews.Infrastructure/Options/AccountConfirmationOptions.cs
+++ src/AnonymousStudentReviews.Infrastructure/Options/AccountConfirmationOptions.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AnonymousStudentReviews.Infrastructure.Options;
 
 public class AccountConfirmationOptions
 {
     public const string SectionName = "AccountConfirmation";
 
-    public int EmailVerificationTokenExpirationHours { get; set; }
-    public string SendConfirmationLetterFromAddress { get; set; } = string.Empty;
-    public string ConfirmationLetterSubject { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Missing configuration value for 'AccountConfirmation:EmailVerificationTokenExpirationMinutes'.")]
+    [Range(1, int.MaxValue, ErrorMessage = "The value for 'AccountConfirmation:EmailVerificationTokenExpirationMinutes' must be greater than 0.")]
+    public int EmailVerificationTokenExpirationMinutes { get; init; }
+
+    [Required(ErrorMessage = "Missing configuration value for 'AccountConfirmation:SendConfirmationLetterFromAddress'.")]
+    [EmailAddress(ErrorMessage = "The value for 'AccountConfirmation:SendConfirmationLetterFromAddress' must be a valid email address.")]
+    public string SendConfirmationLetterFromAddress { get; init; } = string.Empty;
+
+    [Required(ErrorMessage = "Missing configuration value for 'AccountConfirmation:ConfirmationLetterSubject'.")]
+    public string ConfirmationLetterSubject { get; init; } = string.Empty;
 }
